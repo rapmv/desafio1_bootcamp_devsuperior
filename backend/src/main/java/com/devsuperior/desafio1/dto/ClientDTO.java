2cc390294b4ef1f45bd7bb1bab26b7490d80f569
Raphael Maciel Vilela <rap_mv@yahoo.com.br>
2021-11-30 10:18:34 -0300

    Challenge finished

--- backend/src/main/java/com/devsuperior/desafio1/dto/ClientDTO.java
+++ backend/src/main/java/com/devsuperior/desafio1/dto/ClientDTO.java
@@ -1,42 +1,39 @@
-package com.devsuperior.desafio1.entities;
+package com.devsuperior.desafio1.dto;
 
 import java.io.Serializable;
 import java.time.Instant;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.devsuperior.desafio1.entities.Client;
 
-@Entity
-@Table (name = "tb_client")
-public class Client implements Serializable {
+public class ClientDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue( strategy = GenerationType.IDENTITY)
+
 	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
-	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
 	
-	public Client() {
-		
+	public ClientDTO() {	
 	}
 
-	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+	public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
 		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
 		this.income = income;
 		this.birthDate = birthDate;
 		this.children = children;
+	}
+	
+	public ClientDTO(Client entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.cpf = entity.getCpf();
+		this.income = entity.getIncome();
+		this.birthDate = entity.getBirthDate();
+		this.children = entity.getChildren();
 	}
 
 	public Long getId() {
@@ -80,7 +77,7 @@ public class Client implements Serializable {
 		this.birthDate = birthDate;
 	}
 	
-
+	
 	public Integer getChildren() {
 		return children;
 	}
--- backend/src/main/java/com/devsuperior/desafio1/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/desafio1/services/ClientService.java
@@ -1,21 +1,87 @@
 package com.devsuperior.desafio1.services;
 
-import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.desafio1.dto.ClientDTO;
 import com.devsuperior.desafio1.entities.Client;
 import com.devsuperior.desafio1.repositories.ClientRepository;
+import com.devsuperior.desafio1.services.exceptions.DatabaseException;
+import com.devsuperior.desafio1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
-	public List<Client> findAll(){
-		return repository.findAll();
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+
+		return list.map(cli -> new ClientDTO(cli));
+
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+
+		entity = repository.save(entity);
+
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+		
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id );
+		}
+	}
+
 	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+
 }
